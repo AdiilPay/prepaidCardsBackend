04e93da8f45a1bbbd3ea348c40d8718edd43fc37
v0ltis <voltis.discord@gmail.com>
2024-12-14 00:28:57 +0100

    Started V2 of endpoints (cards)

--- src/endpoints/cards.ts
+++ src/endpoints/cards.ts
@@ -1,36 +1,27 @@
 import { Router, Request, Response } from 'express';
-import Db from '@utils/db';
-import authenticate from "@utils/auth/authenticate";
 
-import cardSchema from "@utils/interfaces/client/carte";
+import card from "@dbObjects/Card";
+
 const router = Router();
-
-router.get('/cards', authenticate, (req: Request, res: Response) => {
-
-    Db.getInstance().getCards().then((cards) => {
-        res.send(cards);
-    });
-});
-
-
-router.post('/cards', authenticate, (req: Request, res: Response) => {
-    const parsedBody = cardSchema.safeParse(req.body);
-    if (!parsedBody.success) {res.status(400).send(parsedBody.error);return;}
-
-    Db.getInstance().addCard(req.body.prenom, req.body.nom).then((card) => {
-        res.send(card);
-    });
-});
-
-
 
 // Pas d'authentification pour cette route
 // => Permets d'avoir les infos directement en scannant sa carte
 router.get('/cards/:id', (req: Request, res: Response) => {
 
-    Db.getInstance().getCard(req.params.id).then((card) => {
+    const id = Number(req.params.id);
+
+    // On vérifie que l'id est bien un snowflake
+    if (isNaN(id)) {
+        res.status(400).send("Invalid id");
+        return;
+    }
+
+    card.get(id).then((card) => {
         res.send(card);
+    }).catch(() => {
+        res.status(404).send("Card not found");
     });
 });
+
 
 export default router;
