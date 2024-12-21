0544a3b58032bb9706a09772365838351d733a7c
v0ltis <voltis.discord@gmail.com>
2024-12-21 15:04:05 +0100

    Updated return type of token generator

--- src/utils/auth/createTokens.ts
+++ src/utils/auth/createTokens.ts
@@ -2,7 +2,7 @@ import ServerAgent from "@dbObjects/Agent";
 
 import jwt from "jsonwebtoken";
 
-export default function createToken(agent : ServerAgent){
+export default function createToken(agent : ServerAgent): string {
 
     return jwt.sign({id: agent.id}, process.env.SECRET_KEY as string, {expiresIn: '14h'});
 
