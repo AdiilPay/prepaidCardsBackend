b1852b43956d1fd1655b6e7608a385f9cac79e4a
v0ltis <voltis.discord@gmail.com>
2024-12-21 15:03:19 +0100

    Started implementing profile endpoint

---
+++ src/endpoints/profile.ts
@@ -0,0 +1,65 @@
+import { Router, Request, Response } from 'express';
+
+import card from "@dbObjects/Card";
+import profile from "@dbObjects/Profile";
+
+import authenticate from "@utils/auth/authenticate";
+import {toBigInt} from "@utils/parser";
+
+const router = Router();
+
+
+// Pas d'authentification pour cette route
+router.get('/profile/:profileId', async (req: Request, res: Response) => {
+
+    const userId = toBigInt(req.params.profileId);
+
+    if (userId === null) {
+        res.status(400).json({ error: "Invalid profile id" });
+
+    } else {
+        profile.get(userId).then(async (profile) => {
+            if (profile === null) {
+                res.status(404).json({error: "Profile not found"});
+            } else {
+                res.status(200).json(await profile.toJSON());
+            }
+        }).catch((error) => {
+            res.status(400).json({error});
+        });
+    }
+});
+
+
+router.post('/profile/:profileId/card', authenticate, async (req: Request, res: Response) => {
+
+    const userId = toBigInt(req.params.profileId);
+
+    if (userId === null) {
+        res.status(400).json({ error: "Invalid profile id" });
+
+    } else {
+        profile.get(userId).then(async (profile) => {
+            if (profile === null) {
+                res.status(404).json({error: "Profile not found"});
+            } else {
+                card.create(profile).then(async (card) => {
+                    res.status(200).json(await card.toJSON());
+
+                }).catch((error) => {
+                    console.log(error);
+                    res.status(400).json({error});
+                });
+            }
+
+        }).catch((error) => {
+            console.log(error);
+            res.status(400).json({error});
+        });
+    }
+
+});
+
+
+
+export default router;
