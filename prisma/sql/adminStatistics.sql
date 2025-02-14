020e5be064f68b9e66d4114f26348c1f1c189d58
v0ltis <voltis.discord@gmail.com>
2025-02-14 15:52:39 +0100

    Added new Prisma functions

---
+++ prisma/sql/adminStatistics.sql
@@ -0,0 +1,17 @@
+-- @param {String} $1:organizationId
+-- @param {String} $2:adminId
+-- @param {DateTime} $3:startDate
+-- @param {DateTime} $4:endDate
+
+SELECT
+    DATE(date)                                AS date,
+    SUM(IF(type = 'PAYMENT', ABS(amount), 0)) AS payments,
+    SUM(IF(type = 'REFUND', amount, 0))  AS refunds,
+    SUM(IF(type = 'DEPOSIT', amount, 0))           AS deposits,
+    SUM(IF(type = 'WITHDRAWAL', amount, 0))        AS withdrawals,
+    SUM(IF(type = 'SYSTEM', amount, 0))            AS other
+FROM prisma.Transaction INNER JOIN prisma.Admin ON prisma.Transaction.adminId = prisma.Admin.id
+WHERE prisma.Admin.organizationId = ?
+  AND prisma.Admin.id = ?
+  AND DATE(date) BETWEEN DATE(?) AND DATE(?)
+GROUP BY DATE(date);
