f69273f96bb8e8da0d62adc45585ddc5282d9668
v0ltis <voltis.discord@gmail.com>
2025-02-20 14:29:10 +0100

    Update environment variables and modify token expiration logic

--- src/utils/auth/createTokens.ts
+++ src/utils/auth/createTokens.ts
@@ -4,6 +4,8 @@ import {Admin} from "@prisma/client";
 
 export default function createToken(admin : Admin): string {
 
-    return jwt.sign({id: admin.id}, process.env.SECRET_KEY as string, {expiresIn: '14h'});
+    const tokenExp = process.env.NO_TOKEN_EXPIRATION as unknown as boolean ? '3650d' : '14h';
+
+    return jwt.sign({id: admin.id}, process.env.SECRET_KEY as string, {expiresIn: tokenExp});
 
 }
