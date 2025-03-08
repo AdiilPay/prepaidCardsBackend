a8aac3afb9afc213f02ceec5ea7c7372d48778bf
v0ltis <voltis.discord@gmail.com>
2025-03-08 14:47:23 +0100

    Refactor transaction handling to improve error handling and simplify balance updates

--- src/endpoints/transaction.ts
+++ src/endpoints/transaction.ts
@@ -15,6 +15,7 @@ import asyncHandler from "@utils/asyncHandler";
 import InsufficientBalanceError from "@errors/insufficientBalanceError";
 import DisabledCardUseError from "@errors/DisabledCardUseError";
 import deepTransformDecimals from "@utils/deepTransformDecimals";
+import UnprocessableError from "@errors/UnprocessableError";
 
 type TransactionForm = z.infer<typeof transactionBody>;
 
@@ -24,16 +25,10 @@ router.post('/card/:cardid/transaction', authenticate, validate(transactionBody)
         const card = await prismaClient.card.findUnique({
             where: {
                 id: req.params.cardid,
-                user: {
-                    organizationId: req.admin!.organizationId,
-                    deleted: false
-                }
+                organizationId: req.admin!.organizationId,
+                enabled: true
             },
-            include: {
-                user: true
-            },
-
-        }) as (Card & { user: User }) | null;
+        });
 
         if (card === null) {
             throw new NotFoundError();
@@ -43,37 +38,40 @@ router.post('/card/:cardid/transaction', authenticate, validate(transactionBody)
             throw new DisabledCardUseError();
         }
 
-        if (card.user.balance.toNumber() < req.body.amount && req.body.amount < 0) {
+        if (card.balance.toNumber() < req.body.amount && req.body.amount < 0) {
             throw new InsufficientBalanceError();
         }
 
-        const newBalance = card.user.balance.toNumber() + req.body.amount;
+        if (req.body.type in ["PAYMENT", "WITHDRAWAL"] && req.body.amount > 0) {
+            throw new UnprocessableError(req.body.type + " transactions must have a negative amount");
+        } else if (req.body.type in ["DEPOSIT", "REFUND"] && req.body.amount < 0) {
+            throw new UnprocessableError(req.body.type + " transactions must have a positive amount");
+        }
 
         const data = await prismaClient.$transaction([
-            prismaClient.user.update({
-                where: {
-                    id: card.user.id
-                },
-                data: {
-                    balance: newBalance
-                }
-            }),
-            prismaClient.transaction.create({
-                data: {
-                    amount: req.body.amount,
-                    type: req.body.type,
-                    description: req.body.description,
-                    cardId: card.id,
-                    adminId: req.admin!.id,
-                },
-                include: {
-                    card: {
-                        include: {
-                            user: true
+                prismaClient.card.update({
+                    where: {
+                        id: card.id
+                    },
+                    data: {
+                        balance: {
+                            increment: req.body.amount
                         }
                     }
-                }
-            })
+                }),
+
+                prismaClient.transaction.create({
+                    data: {
+                        amount: req.body.amount,
+                        type: req.body.type,
+                        description: req.body.description,
+                        cardId: card.id,
+                        adminId: req.admin!.id,
+                    },
+                    include: {
+                        card: true
+                    }
+                })
             ]
         )
         res.status(201)
@@ -108,40 +106,6 @@ router.get('/card/:cardId/transactions',
                 date: 'desc'
             }
         });
-
-        res.status(200);
-        res.json(deepTransformDecimals(transactions));
-    }));
-
-router.get('/user/:userId/transactions',
-    asyncHandler(async (req: AuthenticatedRequest, res: Response) => {
-
-        const start = Number(req.query.start) || 0;
-        const count = Number(req.query.count) || 10;
-
-        const user = await prismaClient.user.findUnique({
-            where: {
-                id: req.params.userId,
-            }
-        });
-
-        if (user === null) {
-            throw new NotFoundError();
-        }
-
-        const transactions = await prismaClient.transaction.findMany({
-                where: {
-                    card: {
-                        userId: req.params.userId,
-                    }
-                },
-                skip: start,
-                take: count,
-
-                orderBy: {
-                    date: 'desc'
-                }
-            });
 
         res.status(200);
         res.json(deepTransformDecimals(transactions));
